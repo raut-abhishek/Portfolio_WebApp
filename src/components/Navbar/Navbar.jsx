1c278fed308b0f185c95b90cc2540b312dfc6536
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-23 23:58:53 +0530

    designed navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react'
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+const Navbar = () => {
+
+  const [activeSection, setActiveSection] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // check scroll and change navbar background
+  useEffect(()=>{
+    const handelScroll = (sectionId)=>{
+      setIsScrolled(window.scrollY > 50)
+    }
+    window.addEventListener("scroll", handelScroll);
+    return ()=>{window.removeEventListener("scroll", handelScroll)};
+  },[]);
+
+    // Scroll function
+  const handleMenuItemClick = (sectionId)=>{
+    setActiveSection(sectionId);
+    setIsOpen(false);
+  }
+
+  const menuItems = [
+    { id:"about", label:"About" },
+    { id:"skills", label:"Skills" },
+    { id:"experience", label:"Experience" },
+    { id:"work", label:"Work" },
+    { id:"education", label:"Education" }
+  ]
+
+  return (
+    <>
+    <nav className= {`fixed top-0 w-full z-50 transition duration-300 px-[7vw] lg:px-[20vw] ${
+        isScrolled ? "bg-[#0500414] bg-opacity-50 backdrop-blur-md shadow-md" : "bg-transparent"
+        }`}>
+        <div className='text-white py-5 flex justify-between items-center'>
+
+          {/* Logo */}
+          <div className='lext-lg font-semibold cursor-pointer'>
+            <span className='text-[#8245ec]'>&lt;</span>
+            <span className='text-white'>Abhishek</span>
+            <span className='text-[#8245ec]'>/</span>
+            <span className='text-white'>Raut</span>
+            <span className='text-[#8245ec]'>&gt;</span>
+          </div>
+
+          {/* desktop menu*/}
+          <ul className="hidden md:flex space-x-8 text-gray-300">
+            {menuItems.map((item) =>(
+              <li key={item.id} className = {`cursor-pointer hover:text-[#8245ec] ${
+                activeSection === item.id ? "text-[#8245ec]" : ""
+              }`}>
+                <button onClick={()=>{handleMenuItemClick(item.id)}}>
+                  {item.label}
+
+                </button>
+              </li>
+            ))}
+          </ul>
+          {/* Socoal media icons */}
+          <div className='hidden md:flex space-x-4'>
+            <a href="https://github.com/raut-abhishek" target='_blank' rel='noopener noreferrer' className='text-gray-300 hover:text-[#8245ec]'>
+              <FaGithub size={24}/>
+            </a>
+            <a href="http://linkedin.com/in/abhishek-raut-736a3a21a" target='_blank' rel='noopener noreferrer' className='text-gray-300 hover:text-[#8245ec]'>
+              <FaLinkedin size={24}/>
+            </a>
+          </div>
+            {/* Mobile Menu icons */}
+            <div className='md:hidden'>
+              {
+                isOpen ? (
+                  <FiX className = 'text-3xl text-[#8245ec] cursor-pointer'
+                   onClick ={()=>setIsOpen(false)} />
+                  ) :  (
+                  <FiMenu className = 'text-3xl text-[#8245ec] cursor-pointer'
+                   onClick ={()=>setIsOpen(true)} /> // not working
+                )
+              }
+            </div>
+        </div>
+
+          {/*  Mobile menu items*/}
+        {isOpen &&(
+          <div className='absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414]/50 backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden'>
+            <ul className='flex flex-col items-center space-y-4 py-4 text-gray-300'>
+                {menuItems.map((item)=>{
+                  <li 
+                    key ={item.id} 
+                    className ={`cursor-pointer hover:text-white ${
+                      activeSection === item.id ? "text-[#8245ec]" : ""
+                    }`}>
+                      <button></button>
+                  </li>
+                })}
+            </ul>
+          </div>
+
+        )}
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
