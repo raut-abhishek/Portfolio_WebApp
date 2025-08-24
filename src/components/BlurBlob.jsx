4bba9306c92063707c3a64dbfdd2e26e870a090c
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-08-24 17:42:25 +0530

    designed BlurBlob effect

---
+++ src/components/BlurBlob.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const BlurBlob = ({ position, size }) => {
+  const { top, left } = position;
+  const { width, height } = size;
+
+  return (
+    <div
+      className="absolute"
+      style={{
+        top,
+        left,
+        width,
+        height,
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <div className="w-full h-full rounded-full opacity-30 blur-3xl animate-blob bg-purple-500 z-10"></div>
+    </div>
+  );
+};
+
+BlurBlob.propTypes = {
+  position: PropTypes.shape({
+    top: PropTypes.string,
+    left: PropTypes.string,
+  }),
+  size: PropTypes.shape({
+    width: PropTypes.string,
+    height: PropTypes.string,
+  }),
+};
+
+export default BlurBlob;
